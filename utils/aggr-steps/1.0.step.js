426aecaa9cc0d302aaf3375081c7dac017b1873f
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-06 12:00:22 +0500

    Update 1.0.step.js

--- utils/aggr-steps/1.0.step.js
+++ utils/aggr-steps/1.0.step.js
@@ -2,14 +2,20 @@
 
 
 import 'dotenv/config'
-
+import { makeDirectory } from 'make-dir'
 import { downloadCollection } from '../downloadFromGit.js'
 const localRootFolder = 'data/1.0.step-data'
 const gitRepo = 'crawler-state-2'
 const folders = [
-    //'sponsor-product'
-    ,'sponsor-product-1']
+    'sponsor-product'
+    , 'sponsor-product-1']
 debugger
+
+for (let pth of folders) {
+
+    await makeDirectory(`${process.cwd()}/data/1.0.step-data/uzipped-data/${pth}`)
+    await makeDirectory(`${process.cwd()}/data/1.0.step-data/zipped-data/${pth}`)
+}
 for (let g of folders) {
 
     const gitFolder = g
