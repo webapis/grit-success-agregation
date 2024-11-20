aa48012af009af452b0bdec6cc9970c9f0a7ca3f
Serdar Ashirov <webapis@users.noreply.github.com>
2024-11-20 10:29:16 +0500

    d

--- utils/aggr-steps/1.0.step.js
+++ utils/aggr-steps/1.0.step.js
@@ -4,40 +4,16 @@
 import 'dotenv/config'
 import { makeDirectory } from 'make-dir'
 import { downloadCollection } from '../downloadFromGit.js'
+import foldersTodownload from '../foldersTodownload.js'
 const localRootFolder = 'data/1.0.step-data'
 const gitRepo = 'crawler-state-2'
-const dev = (process.env.LOCAL==='true')
-const folders = dev? [
+const dev = (process.env.LOCAL === 'true')
+const folders = dev ? [
   'sponsor-product-21',  //marksandspencer
   'sponsor-product-23'   //paulmark
-] :[
-  'sponsor-ayakkabi',
-  'sponsor-product',
-  'sponsor-product-1',
-  'sponsor-product-2',//koton
-  'sponsor-product-3',//mango
-  'sponsor-product-4',//beymen
-  'sponsor-product-5',//hmdtektil
-  'sponsor-product-6',//matras
-  'sponsor-product-7',//ateliergalin
-  'sponsor-product-8',//diesel
-  'sponsor-product-9',//uspoloassn
-  'sponsor-product-10',//gap
-  'sponsor-product-11',//burberry
-  'sponsor-product-13',//camper
-  'sponsor-product-14',//converse
-  'sponsor-product-15',//eclecticconceptstore
-  'sponsor-product-17',//gant
-  'sponsor-product-18',//lacoste
-  'sponsor-product-19',//pierrecardin
-  'sponsor-product-24',//morrez 
-  'sponsor-product-25',//yaygan
-  'sponsor-product-M-4',//mudo
-  'sponsor-product-M-5',//muun
-  'sponsor-product-M-6'//sagaza 
-  ]
+] : foldersTodownload
 
-  console.log('folders to download:',folders.length)
+console.log('folders to download:', folders.length)
 debugger
 
 for (let pth of folders) {
---
+++ utils/foldersTodownload.js
@@ -0,0 +1,26 @@
+export default [
+    'sponsor-ayakkabi',
+    'sponsor-product',
+    'sponsor-product-1',
+    'sponsor-product-2',//koton
+    'sponsor-product-3',//mango
+    'sponsor-product-4',//beymen
+    'sponsor-product-5',//hmdtektil
+    'sponsor-product-6',//matras
+    'sponsor-product-7',//ateliergalin
+    'sponsor-product-8',//diesel
+    'sponsor-product-9',//uspoloassn
+    'sponsor-product-10',//gap
+    'sponsor-product-11',//burberry
+    'sponsor-product-13',//camper
+    'sponsor-product-14',//converse
+    'sponsor-product-15',//eclecticconceptstore
+    'sponsor-product-17',//gant
+    'sponsor-product-18',//lacoste
+    'sponsor-product-19',//pierrecardin
+    'sponsor-product-24',//morrez 
+    'sponsor-product-25',//yaygan
+    'sponsor-product-M-4',//mudo
+    'sponsor-product-M-5',//muun
+    'sponsor-product-M-6'//sagaza 
+    ]
